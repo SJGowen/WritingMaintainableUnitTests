3bdb4ddc6feb2d6304984866b2444ee0c18599fc
Stephen Gowen <stephen.gowen@civica.co.uk>
2023-10-03 11:15:18 +0100

    Used file-scoped namespace (added ; at the end of namespace line).

--- WritingMaintainableUnitTests.Tests/Module1_TypesOfTests/StateVerification/ResumeTests.cs
+++ WritingMaintainableUnitTests.Tests/Module1_TypesOfTests/StateVerification/ResumeTests.cs
@@ -1,29 +1,27 @@
-﻿using System;
+﻿namespace WritingMaintainableUnitTests.Tests.Module1_TypesOfTests.StateVerification;
+
+using System;
 using System.Linq;
 using NUnit.Framework;
 using WritingMaintainableUnitTests.Module1_TypesOfTests.StateVerification;
-// ReSharper disable InconsistentNaming
 
-namespace WritingMaintainableUnitTests.Tests.Module1_TypesOfTests.StateVerification
+[TestFixture]
+public class When_adding_experience_to_a_resume
 {
-    [TestFixture]
-    public class When_adding_experience_to_a_resume
+    [Test]
+    public void Then_the_specified_experience_should_now_appear_on_the_resume()
     {
-        [Test]
-        public void Then_the_specified_experience_should_now_appear_on_the_resume()
-        {
-            var experienceFrom = new DateTime(2014, 09, 12);
-            var experienceUntil = new DateTime(2017, 12, 31);
-            
-            var resume = new Resume();
-            resume.AddExperience("Google", "Data analyst", experienceFrom, experienceUntil);
+        var experienceFrom = new DateTime(2014, 09, 12);
+        var experienceUntil = new DateTime(2017, 12, 31);
+        
+        var resume = new Resume();
+        resume.AddExperience("Google", "Data analyst", experienceFrom, experienceUntil);
 
-            var addedExperience = resume.Experiences.SingleOrDefault();
-            Assert.That(addedExperience, Is.Not.Null);
-            Assert.That(addedExperience.Employer, Is.EqualTo("Google"));
-            Assert.That(addedExperience.Role, Is.EqualTo("Data analyst"));
-            Assert.That(addedExperience.From, Is.EqualTo(experienceFrom));
-            Assert.That(addedExperience.Until, Is.EqualTo(experienceUntil));
-        }
+        var addedExperience = resume.Experiences.SingleOrDefault();
+        Assert.That(addedExperience, Is.Not.Null);
+        Assert.That(addedExperience.Employer, Is.EqualTo("Google"));
+        Assert.That(addedExperience.Role, Is.EqualTo("Data analyst"));
+        Assert.That(addedExperience.From, Is.EqualTo(experienceFrom));
+        Assert.That(addedExperience.Until, Is.EqualTo(experienceUntil));
     }
 }
--- WritingMaintainableUnitTests/Module1_TypesOfTests/StateVerification/Resume.cs
+++ WritingMaintainableUnitTests/Module1_TypesOfTests/StateVerification/Resume.cs
@@ -1,38 +1,37 @@
-﻿using System;
+﻿namespace WritingMaintainableUnitTests.Module1_TypesOfTests.StateVerification;
+
+using System;
 using System.Collections.Generic;
 
-namespace WritingMaintainableUnitTests.Module1_TypesOfTests.StateVerification
+public class Resume
 {
-    public class Resume
+    private readonly IList<Experience> _experiences;
+    public IEnumerable<Experience> Experiences => _experiences;
+
+    public Resume()
     {
-        private readonly IList<Experience> _experiences;
-        public IEnumerable<Experience> Experiences => _experiences;
-
-        public Resume()
-        {
-            _experiences = new List<Experience>();
-        }
-
-        public void AddExperience(string employer, string role, DateTime from, DateTime until)
-        {            
-            var newExperience = new Experience(employer, role, from, until);
-            _experiences.Add(newExperience);
-        }
+        _experiences = new List<Experience>();
     }
 
-    public class Experience
-    {
-        public string Employer { get; }
-        public string Role { get; }
-        public DateTime From { get; }
-        public DateTime Until { get; }
+    public void AddExperience(string employer, string role, DateTime from, DateTime until)
+    {            
+        var newExperience = new Experience(employer, role, from, until);
+        _experiences.Add(newExperience);
+    }
+}
 
-        public Experience(string employer, string role, DateTime from, DateTime until)
-        {
-            Employer = employer;
-            Role = role;
-            From = from;
-            Until = until;
-        }
+public class Experience
+{
+    public string Employer { get; }
+    public string Role { get; }
+    public DateTime From { get; }
+    public DateTime Until { get; }
+
+    public Experience(string employer, string role, DateTime from, DateTime until)
+    {
+        Employer = employer;
+        Role = role;
+        From = from;
+        Until = until;
     }
 }
